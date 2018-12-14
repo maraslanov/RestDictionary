ef56d5469b3ed116efc6dd051d09eab7fc0869ea
Арасланов Максим <maraslanov@diasoft.ru>
2018-12-14 12:10:22 +0500

    Initial commit

---
+++ src/main/java/com/example/dictionary/RestDictionaryApplication.java
@@ -0,0 +1,14 @@
+package com.example.dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDictionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestDictionaryApplication.class, args);
+    }
+
+}
+
