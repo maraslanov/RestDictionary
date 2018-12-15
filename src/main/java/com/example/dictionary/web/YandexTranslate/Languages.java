26691eb274d79086aaad5a10e38d8473e57b704d
Арасланов Максим <maraslanov@diasoft.ru>
2018-12-15 19:44:44 +0500

    add api for yandex translate

---
+++ src/main/java/com/example/dictionary/web/YandexTranslate/Languages.java
@@ -0,0 +1,50 @@
+package com.example.dictionary.web.YandexTranslate;
+
+public enum Languages {
+    ALBANIAN("sq"),
+    ARMENIAN("hy"),
+    AZERBAIJANI("az"),
+    BELARUSIAN("be"),
+    BULGARIAN("bg"),
+    CATALAN("ca"),
+    CROATIAN("hr"),
+    CZECH("cs"),
+    DANISH("da"),
+    DUTCH("nl"),
+    ENGLISH("en"),
+    ESTONIAN("et"),
+    FINNISH("fi"),
+    FRENCH("fr"),
+    GERMAN("de"),
+    GEORGIAN("ka"),
+    GREEK("el"),
+    HUNGARIAN("hu"),
+    ITALIAN("it"),
+    LATVIAN("lv"),
+    LITHUANIAN("lt"),
+    MACEDONIAN("mk"),
+    NORWEGIAN("no"),
+    POLISH("pl"),
+    PORTUGUESE("pt"),
+    ROMANIAN("ro"),
+    RUSSIAN("ru"),
+    SERBIAN("sr"),
+    SLOVAK("sk"),
+    SLOVENIAN("sl"),
+    SPANISH("es"),
+    SWEDISH("sv"),
+    TURKISH("tr"),
+    UKRAINIAN("uk");
+
+    private final String language;
+
+    private Languages(final String pLanguage) {
+        language = pLanguage;
+    }
+
+    @Override
+    public String toString() {
+        return language;
+    }
+
+}
